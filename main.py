a7f4f9cbf94645004cedb99ddb1f7f2b811ca277
Matthew Eadie <MEadie@dundee.ac.uk>
2024-05-17 12:36:37 +0100

    Camera working and sending to display

--- main.py
+++ main.py
@@ -337,13 +337,13 @@ class Window(QWidget):
         if displayMode == 1:
             self.globalDisplayMode = 1
             self.MLThread.start()
-            self.processingThread.start()
+            # self.processingThread.start()
             self.cameraThread.exit() #End camera thread when swapping to playback
             print('Machine learning mode')
         elif displayMode == 2:
             self.globalDisplayMode = 2
             self.cameraThread.start() #Creates thread for camera acquisition
-            self.processingThread.exit() #End playback thread
+            # self.processingThread.exit() #End playback thread
             print('Acquisition mode')
         else:
             print('Error setting display mode')
@@ -572,10 +572,10 @@ class Window(QWidget):
         groupExposureSetLayout.addWidget(self.radioExposure50)
 
         #Connect radio buttons to functions
-        self.radioExposure5.toggled.connect(self.createTriggerTask)
-        self.radioExposure15.toggled.connect(self.createTriggerTask)
-        self.radioExposure25.toggled.connect(self.createTriggerTask)
-        self.radioExposure50.toggled.connect(self.createTriggerTask)
+        # self.radioExposure5.toggled.connect(self.createTriggerTask)
+        # self.radioExposure15.toggled.connect(self.createTriggerTask)
+        # self.radioExposure25.toggled.connect(self.createTriggerTask)
+        # self.radioExposure50.toggled.connect(self.createTriggerTask)
 
         #Add exposure settings group to tab layout
         self.cameraSettingsLayout.addWidget(groupExposureSettings)
@@ -783,55 +783,17 @@ class Window(QWidget):
         self.MLPipeline.currentImageUpdated(self.currentImageNo)
 
     def initaliseCamera(self):
-        # #CAMERA INITALISATION
-        # if self.cameraFunctions.initialise(): #Initialise camera
-        #         self.cameraInitialised = True
-        # else:
-        #     self.createErrorMessage('No camera detected, starting without acqisition mode.')
-        #     # print('Error initalising camera!')
-        #     self.cameraInitialised = False
-        #     self.disableCameraOptions()
-        #     #ADD FUNCTION TO DISABLE CAMERA SETTING IF NOT INITIALISED
+        #CAMERA INITALISATION
+        if self.cameraFunctions.initialise(): #Initialise camera
+                self.cameraInitialised = True
+        else:
+            self.createErrorMessage('No camera detected, starting without acqisition mode.')
+            # print('Error initalising camera!')
+            self.cameraInitialised = False
+            self.disableCameraOptions()
+            #ADD FUNCTION TO DISABLE CAMERA SETTING IF NOT INITIALISED
         pass
 
-    def createTriggerTask(self):
-        # try:
-            # try:
-            #     self.task.close() #clear any previous task
-            # except:
-            #     print('no task to close')
-
-            # newExposure = self.btngrpExposure.checkedId()/1000 #change to milliseconds
-            # print(newExposure)
-
-            # self.task = nidaqmx.Task()
-            # self.task.co_channels.add_co_pulse_chan_time("cDAQ_VER/ctr0",initial_delay=0.0, low_time=newExposure, high_time=newExposure) #I/O channel 0 
-            # self.task.co_channels.add_co_pulse_chan_time("cDAQ_VER/ctr1",initial_delay=0.0, low_time=newExposure, high_time=newExposure) #I/O channel 3
-            # # task.co_channels.add_co_pulse_chan_time("cDAQ_VER/ctr2",initial_delay=0.0, low_time=0.5, high_time=0.5) #I/O channel 1 
-            # # task.co_channels.add_co_pulse_chan_time("cDAQ_VER/ctr3",initial_delay=0.1, low_time=0.15, high_time=0.05) I/O channel 2
-            # self.task.timing.cfg_implicit_timing(sample_mode=AcquisitionType.CONTINUOUS)
-            # # self.task.timing.cfg_implicit_timing(sample_mode=AcquisitionType.FINITE, samps_per_chan=10)
-            # print('trigger set')
-        # except:
-        #     self.txtInfo.setText('Error setting trigger task.')
-        print('Error setting trigger task.')
-
-    def startTriggering(self):
-        try:
-            self.task.start()
-            print('starting trigger')
-        except:
-            print('Error start Task')
-
-    def stopTriggering(self):
-        try:
-            self.task.stop() #clear any previous task
-        except:
-            print('Task could not be stopped')
-
-        # while(not(self.task.is_task_done())):
-        #         self.task.is_task_done()
-        
 
 
 
